d6c6e8122321694436acaefedd81739d1a9116b5
Josué Lobo <darksiders20005@gmail.com>
2022-01-14 13:45:36 -0300

    endpoint de talker feito

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,6 +12,12 @@ const PORT = '3000';
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+app.get('/talker', async (_req, res) => {
+  const fileConteiner = await fs.readFile('./talker.json', 'utf8')
+    .then((e) => JSON.parse(e))
+  res.status(200).send(fileConteiner);
+})
 
 app.listen(PORT, () => {
   console.log('Online');
