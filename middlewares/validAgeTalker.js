28c423de8ae6df15248fb6ef8770f31a9c5c5c27
Josué Lobo <darksiders20005@gmail.com>
2022-01-15 15:34:44 -0300

    validações de idade adicionadas

---
+++ middlewares/validAgeTalker.js
@@ -0,0 +1,20 @@
+function validAgeUser(req, res, next) {
+  const { age } = req.body;
+  if (age < 18) {
+    res.status(400).json({ message: "A pessoa palestrante deve ser maior de idade"})
+  }
+  next();
+}
+
+  function validAgeNotNull(req, res, next) {
+  const { age } = req.body;
+  if (!age) {
+    res.status(400).json({ message: "O campo 'age' é obrigatório"})
+  }
+  next();
+}
+
+module.exports = {
+  validAgeUser,
+  validAgeNotNull
+};
