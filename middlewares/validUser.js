39cc6ca86551cf4d0d70f4ba7734198ce9e256bf
Josué Lobo <darksiders20005@gmail.com>
2022-01-17 22:50:00 -0300

    unindo middlewares de nome e idade em um só modulo de usuario

---
+++ middlewares/validUser.js
@@ -0,0 +1,22 @@
+function validName(req, res, next) {
+  const { name } = req.body;
+  if (!name) return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  next();
+}
+
+function validAge(req, res, next) {
+  const { age } = req.body;
+  if (!age) return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  if (age < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  next();
+}
+
+module.exports = {
+  validAge,
+  validName,
+};
