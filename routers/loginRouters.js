1d4ac9c4dc0bb585bc871847bb15227748608a37
Josué Lobo <darksiders20005@gmail.com>
2022-01-17 22:27:44 -0300

    modificando de app para router em login

--- routers/loginRouters.js
+++ routers/loginRouters.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const TOKEN = generateToken();
 const loginAuthentication = require('../middlewares/loginAuthentication');
 
-app.post('/login', loginAuthentication, (req, res) => {
+router.post('/login', loginAuthentication, (req, res) => {
   req.header = { ...req.header, authorization: TOKEN };
   return res.status(200).json({ token: TOKEN });
 });
